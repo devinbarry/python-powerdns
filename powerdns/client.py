be9d2b28f5b33330aa93b16f112af09c78c0d535
Devin Barry <devinbarry@users.noreply.github.com>
2024-07-08 12:24:26 +0100

    Clean up and improve code in the client class.

--- powerdns/client.py
+++ powerdns/client.py
@@ -1,77 +1,22 @@
-# -*- coding: utf-8 -*-
-#
-#  PowerDNS web api python client and interface (python-powerdns)
-#
-#  Copyright (C) 2018 Denis Pompilio (jawa) <denis.pompilio@gmail.com>
-#
-#  This file is part of python-powerdns
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the MIT License.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  MIT License for more details.
-#
-#  You should have received a copy of the MIT License along with this
-#  program; if not, see <https://opensource.org/licenses/MIT>.
-
-"""
-powerdns.client - PowerDNS API client
-"""
-
 import json
 import logging
-from functools import partial
 import requests
 from .exceptions import PDNSError
-
 
 LOG = logging.getLogger(__name__)
 
 
-# pylint: disable=too-many-instance-attributes
 class PDNSApiClient:
     """Powerdns API client
 
     It implements common HTTP methods GET, POST, PUT, PATCH and DELETE
 
-    This client is using :mod:`requests` package. Please see
-    http://docs.python-requests.org/ for more information.
-
     :param str api_endpoint: Powerdns API endpoint
     :param str api_key: API key
     :param bool verify: Control SSL certificate validation
     :param int timeout: Request timeout in seconds
-
-    .. method:: get(self, path, data=None, **kwargs)
-
-        Partial method invoking :meth:`~PDNSApiClient.request` with
-        http method *GET*.
-
-    .. method:: post(self, path, data=None, **kwargs)
-
-        Partial method invoking :meth:`~PDNSApiClient.request` with
-        http method *POST*.
-
-    .. method:: put(self, path, data=None, **kwargs)
-
-        Partial method invoking :meth:`~PDNSApiClient.request` with
-        http method *PUT*.
-
-    .. method:: patch(self, path, data=None, **kwargs)
-
-        Partial method invoking :meth:`~PDNSApiClient.request` with
-        http method *PATCH*.
-
-    .. method:: delete(self, path, data=None, **kwargs)
-
-        Partial method invoking :meth:`~PDNSApiClient.request` with
-        http method *DELETE*.
     """
     def __init__(self, api_endpoint, api_key, verify=True, timeout=None):
-        """Initialization"""
         self._api_endpoint = api_endpoint
         self._api_key = api_key
         self._verify = verify
@@ -79,27 +24,16 @@ class PDNSApiClient:
 
         if not verify:
             LOG.debug("removing insecure https connection warnings")
-            requests.urllib3.disable_warnings(
-                requests.urllib3.exceptions.InsecureRequestWarning
-            )
+            requests.urllib3.disable_warnings(requests.urllib3.exceptions.InsecureRequestWarning)
 
         self.request_headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
 
-        # Directly expose common HTTP methods
-        self.get = partial(self.request, method='GET')
-        self.post = partial(self.request, method='POST')
-        self.put = partial(self.request, method='PUT')
-        self.patch = partial(self.request, method='PATCH')
-        self.delete = partial(self.request, method='DELETE')
-
     def __repr__(self):
-        return "PDNSApiClient(%s, %s, verify=%s, timeout=%s)" % (
-            repr(self._api_endpoint), repr(self._api_key),
-            repr(self._verify), repr(self._timeout)
-        )
+        return (f"PDNSApiClient({repr(self._api_endpoint)}, {repr(self._api_key)}, verify={repr(self._verify)}, "
+                f"timeout={repr(self._timeout)})")
 
     def __str__(self):
         return self._api_endpoint
@@ -124,7 +58,7 @@ class PDNSApiClient:
         if not path.startswith('http://') and not path.startswith('https://'):
             if path.startswith('/'):
                 path = path.lstrip('/')
-            url = "%s/%s" % (self._api_endpoint, path)
+            url = f"{self._api_endpoint}/{path}"
         else:
             url = path
 
@@ -145,8 +79,6 @@ class PDNSApiClient:
         LOG.info("request response code: %d", response.status_code)
         LOG.debug("response: %s", response.text)
 
-        # Try to handle basic return
-        # pylint: disable=no-else-return
         if response.status_code in [200, 201]:
             return response.json()
         elif response.status_code == 204:
@@ -179,3 +111,23 @@ class PDNSApiClient:
         else:
             err = 'No error message found'
         return err
+
+    def get(self, path, data=None, **kwargs):
+        """Perform GET request"""
+        return self.request(path, method='GET', data=data, **kwargs)
+
+    def post(self, path, data=None, **kwargs):
+        """Perform POST request"""
+        return self.request(path, method='POST', data=data, **kwargs)
+
+    def put(self, path, data=None, **kwargs):
+        """Perform PUT request"""
+        return self.request(path, method='PUT', data=data, **kwargs)
+
+    def patch(self, path, data=None, **kwargs):
+        """Perform PATCH request"""
+        return self.request(path, method='PATCH', data=data, **kwargs)
+
+    def delete(self, path, data=None, **kwargs):
+        """Perform DELETE request"""
+        return self.request(path, method='DELETE', data=data, **kwargs)
